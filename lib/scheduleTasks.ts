1d9462faaaa1c3ed456b2caba74c6f1a7312953a
NSU-Turag <abdullah.turag@northsouth.edu>
2024-12-01 21:50:23 +0600

    empth schedule task

--- lib/scheduleTasks.ts
+++
@@ -1,7 +0,0 @@
-import { schedule } from 'node-schedule'
-import { JobCallback, Spec } from 'node-schedule'
-
-//console log in every 5 seconds
-schedule('*/5 * * * * *', () => {
-    console.log('This job runs every 5 seconds to log a message')
-})
